f429a693d0e2a9248c5b1f6967fb37b94c34419e
Rody Castellanos <rodycastellanosrodriguez@gmail.com>
2025-09-09 11:44:40 -0600

    Tarea completada

--- task4.js
+++ task4.js
@@ -3,12 +3,7 @@ export function delUser(id) {
   fetch(`http://localhost:3000/users/${id}`, { 
     method: "DELETE",
   })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`No se pudo borrar el usuario con id ${id}.`);
-      }
-      console.log(`Usuario con id ${id} eliminado.`);
-    })
+    .then(respuesta => respuesta.json())
     .catch(error => console.error("Error al eliminar usuario:", error.message));
 }
 
