2e5fc78a5a400da4068ba3a0922f33d1c78c7bfe
Rody Castellanos <rodycastellanosrodriguez@gmail.com>
2025-09-09 11:09:18 -0600

    Tarea Completada

--- task3.js
+++ task3.js
@@ -1,2 +1,17 @@
 // Task 3: addUser(first_name, last_name, email)
+export function addUser(first_name, last_name, email){
+  fetch("http://localhost:3000/users",{
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    first_name: first_name,
+    last_name: last_name,
+    email: email
+  }),
+
+   }).then(response => response.json())
+    .catch(error => console.error("Error al obtener usuarios:", error));
+}
 
